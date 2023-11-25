d23859a3d22f52bd847e018538df043ccdeb3754
haton14 <haton14@haton14.com>
2023-11-25 13:43:22 +0900

    Refactor unko and unko.$id routes

--- app/routes/unko.$id.tsx
+++ app/routes/unko.$id.tsx
@@ -1,9 +1,24 @@
 import type { MetaFunction } from '@remix-run/cloudflare'
+import type { LoaderFunctionArgs } from '@remix-run/cloudflare'
+import { json } from '@remix-run/cloudflare'
+import { useLoaderData } from '@remix-run/react'
 
 export const meta: MetaFunction = () => {
   return [{ title: 'New Unko' }, { name: 'description', content: 'Welcome to Remix!' }]
 }
+export async function loader({ request, context }: LoaderFunctionArgs) {
+  return json({ unko: 'unko' })
+}
 
 export default function Index() {
-  return <h1>Unko</h1>
+  const { unko } = useLoaderData<typeof loader>()
+  return (
+    <div>
+      <h1>Unko</h1>
+      <div>Unko</div>
+      {unko}
+      <h1>Unko</h1>
+      <div>Unko</div>
+    </div>
+  )
 }
--- app/routes/unko.tsx
+++ app/routes/unko.tsx
@@ -1,9 +1,24 @@
 import type { MetaFunction } from '@remix-run/cloudflare'
+import type { LoaderFunctionArgs } from '@remix-run/cloudflare'
+import { json } from '@remix-run/cloudflare'
+import { useLoaderData } from '@remix-run/react'
 
 export const meta: MetaFunction = () => {
   return [{ title: 'New Unko' }, { name: 'description', content: 'Welcome to Remix!' }]
 }
+export async function loader({ request, context }: LoaderFunctionArgs) {
+  return json({ unko: 'unko' ,wanko:'wanko'})
+}
 
 export default function Index() {
-  return <h1>Unko</h1>
+  const  user  = useLoaderData<typeof loader>()
+  return (
+    <div>
+      <h1>Unko</h1>
+      <div>wanko</div>
+      <code>{JSON.stringify(user)}</code>
+      <h1>wanko2</h1>
+      <div>wanko3</div>
+    </div>
+  )
 }
